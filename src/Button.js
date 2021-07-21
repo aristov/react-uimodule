322818d4027e788078d446d63aa57d11903b0ce3
Vyacheslav Aristov <vv.aristov@gmail.com>
2021-07-21 14:22:45 +0300

    Button: handling the "active" state during mouse events

--- src/Button.js
+++ src/Button.js
@@ -4,19 +4,43 @@ import './Button.css'
 
 export class Button extends React.Component
 {
+  state = { active : false }
+
   render() {
     return (
       <div
         role="button"
-        className="Button Widget"
+        className={ ['Button Widget', this.state.active && 'active'].filter(Boolean).join(' ') }
         aria-pressed={ this.props.pressed }
         aria-disabled={ this.props.disabled }
         tabIndex={ this.props.disabled? null : 0 }
+        onMouseDown={ this.onMouseDown }
+        onMouseLeave={ this.onMouseLeave }
+        onMouseUp={ this.onMouseUp }
       >
         <Control>
           { this.props.children }
         </Control>
       </div>
     )
+  }
+
+  onMouseDown = () => {
+    if(this.props.disabled) {
+      return
+    }
+    this.setState({ active : true })
+  }
+
+  onMouseLeave = () => {
+    if(this.state.active) {
+      this.setState({ active : false })
+    }
+  }
+
+  onMouseUp = () => {
+    if(this.state.active) {
+      this.setState({ active : false })
+    }
   }
 }
